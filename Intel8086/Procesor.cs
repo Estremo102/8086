abd2d18a990819983f8eb769bce5913809e0df10
Dominik Możdżeń <Dominikmozdzen@gmail.com>
2022-03-10 10:41:20 +0100

    przeciążenie konstruktora

--- Intel8086/Procesor.cs
+++ Intel8086/Procesor.cs
@@ -4,37 +4,68 @@ namespace Intel8086
 {
     public class Procesor
     {
-        string[] rejestr = new string[8];
+        private string[] register = new string[8];
+        public string AH { get => register[0]; private set => register[0] = value; }
+        public string AL { get => register[1]; private set => register[0] = value; }
+        public string BH { get => register[2]; private set => register[0] = value; }
+        public string BL { get => register[3]; private set => register[0] = value; }
+        public string CH { get => register[4]; private set => register[0] = value; }
+        public string CL { get => register[5]; private set => register[0] = value; }
+        public string DH { get => register[6]; private set => register[0] = value; }
+        public string DL { get => register[7]; private set => register[0] = value; }
 
         delegate void Operation(int a, int b);
 
         public Procesor()
         {
             string ah, al, bh, bl, ch, cl, dh, dl;
-            while (!InputData("AH", out ah)) { Console.WriteLine("Podano nieprawidłowe dane"); }
-            while (!InputData("AL", out al)) { Console.WriteLine("Podano nieprawidłowe dane"); }
-            while (!InputData("BH", out bh)) { Console.WriteLine("Podano nieprawidłowe dane"); }
-            while (!InputData("BL", out bl)) { Console.WriteLine("Podano nieprawidłowe dane"); }
-            while (!InputData("CH", out ch)) { Console.WriteLine("Podano nieprawidłowe dane"); }
-            while (!InputData("CL", out cl)) { Console.WriteLine("Podano nieprawidłowe dane"); }
-            while (!InputData("DH", out dh)) { Console.WriteLine("Podano nieprawidłowe dane"); }
-            while (!InputData("DL", out dl)) { Console.WriteLine("Podano nieprawidłowe dane"); }
-            rejestr[0] = ah;
-            rejestr[1] = al;
-            rejestr[2] = bh;
-            rejestr[3] = bl;
-            rejestr[4] = ch;
-            rejestr[5] = cl;
-            rejestr[6] = dh;
-            rejestr[7] = dl;
+            while (!InputData("AH", out ah)) { Console.WriteLine("Wrong data"); }
+            while (!InputData("AL", out al)) { Console.WriteLine("Wrong data"); }
+            while (!InputData("BH", out bh)) { Console.WriteLine("Wrong data"); }
+            while (!InputData("BL", out bl)) { Console.WriteLine("Wrong data"); }
+            while (!InputData("CH", out ch)) { Console.WriteLine("Wrong data"); }
+            while (!InputData("CL", out cl)) { Console.WriteLine("Wrong data"); }
+            while (!InputData("DH", out dh)) { Console.WriteLine("Wrong data"); }
+            while (!InputData("DL", out dl)) { Console.WriteLine("Wrong data"); }
+            register[0] = ah;
+            register[1] = al;
+            register[2] = bh;
+            register[3] = bl;
+            register[4] = ch;
+            register[5] = cl;
+            register[6] = dh;
+            register[7] = dl;
+        }
+
+        public Procesor(params string[] registers)
+        {
+            if (registers.Length != 8) throw new ArgumentException();
+            foreach (var register in registers) if (CheckData(register)) throw new ArgumentException();
+            AH = registers[0];
+            AL = registers[1];
+            BH = registers[2];
+            BL = registers[3];
+            CH = registers[4];
+            CL = registers[5];
+            DH = registers[6];
+            DL = registers[7];
         }
 
         bool InputData(string whatIsInput, out string a)
         {
-            Console.WriteLine("Podaj " + whatIsInput);
+            Console.Write(whatIsInput + ": ");
             a = Console.ReadLine().ToUpper();
             if (a.Length != 2) return false;
             if ((((int)a[0] >= 48 && (int)a[0] <= 57) || ((int)a[0] >= 65 && (int)a[0] <= 70)) && (((int)a[1] >= 48 && (int)a[1] <= 57) || ((int)a[1] >= 65 && (int)a[1] <= 70)))
+                return true;
+            return false;
+        }
+
+        bool CheckData(string data)
+        {
+            data = data.ToUpper();
+            if (data.Length != 2) return false;
+            if ((((int)data[0] >= 48 && (int)data[0] <= 57) || ((int)data[0] >= 65 && (int)data[0] <= 70)) && (((int)data[1] >= 48 && (int)data[1] <= 57) || ((int)data[1] >= 65 && (int)data[1] <= 70)))
                 return true;
             return false;
         }
@@ -104,12 +135,12 @@ namespace Intel8086
             o(RegisterToInt(r1), RegisterToInt(r2));
         }
 
-        void MOV(int a, int b) => rejestr[a] = rejestr[b];
+        void MOV(int a, int b) => register[a] = register[b];
         void XCH(int a, int b)
         {
-            string temp = rejestr[a];
-            rejestr[a] = rejestr[b];
-            rejestr[b] = temp;
+            string temp = register[a];
+            register[a] = register[b];
+            register[b] = temp;
         }
 
         static int RegisterToInt(string r)
@@ -128,12 +159,9 @@ namespace Intel8086
             }
         }
 
-        public override string ToString()
-        {
-            return $"AX: AH[{rejestr[0]}] AL[{rejestr[1]}]\n" +
-                   $"BX: BH[{rejestr[2]}] BL[{rejestr[3]}]\n" +
-                   $"CX: CH[{rejestr[4]}] CL[{rejestr[5]}]\n" +
-                   $"DX: DH[{rejestr[6]}] DL[{rejestr[7]}]";
-        }
+        public override string ToString() => $"AX: AH[{register[0]}] AL[{register[1]}]\n" +
+                                             $"BX: BH[{register[2]}] BL[{register[3]}]\n" +
+                                             $"CX: CH[{register[4]}] CL[{register[5]}]\n" +
+                                             $"DX: DH[{register[6]}] DL[{register[7]}]";
     }
 }
